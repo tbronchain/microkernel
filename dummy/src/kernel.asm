228f4adf4876b5963a730244996a8f5d64f3f962
Thibault BRONCHAIN <thibault@mc2.io>
2015-01-12 16:26:23 +0800

    add bootloader

--- dummy/src/kernel.asm
+++ dummy/src/kernel.asm
@@ -7,6 +7,10 @@
 bits 32                         ;NASM instruction 32bits CPU
 
 section .text                   ;Code starts here
+        align 4                 ;32bits values
+        dd 0x1BADB002           ;Bootloader Magic number
+        dd 0x00                 ;Bootloader Flags
+        dd -(0x1BADB002+0x00)   ;Bootloader Checksum (Opposite of M+F => M+F+C=0)
 
 global kentry                   ;Define being as entry point
 extern kmain                    ;`main` function definition, in external file
